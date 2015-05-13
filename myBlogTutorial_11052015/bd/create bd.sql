d262d328743a5f5aa0e50d099a709a3b40640d70
akbars95 <mynzat.dmitrii@gmail.com>
2015-05-13 14:06:22 +0300

    -delete project

--- myBlogTutorial_11052015/bd/create bd.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE `myBlog` CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'myBlog'@'localhost' IDENTIFIED BY 'myBlog';
-GRANT ALL PRIVILEGES ON * . * TO 'myBlog'@'localhost';
