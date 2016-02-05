b05587ab7d69662122a3575947afda69ee57007c
akbars95 <mynzat.dmitrii@gmail.com>
2016-02-05 12:14:25 +0200

    -Task #212 add support log4j and config

--- souvenir/src/main/java/com/mtsmda/souvenir/controller/StaticPageController.java
+++ souvenir/src/main/java/com/mtsmda/souvenir/controller/StaticPageController.java
@@ -1,11 +1,18 @@
 package com.mtsmda.souvenir.controller;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class StaticPageController {
+
+	private static final Logger logger = Logger.getLogger(StaticPageController.class);
 
 	private static final String ROOT = "/";
 	private static final String INDEX = "index";
@@ -20,34 +27,38 @@ public class StaticPageController {
 
 	private static final String ABOUT_US = "about_us";
 	private static final String ABOUT_US_URL = ROOT + ABOUT_US;
-	
+
 	private static final String CONTACT_US = "contact_us";
 	private static final String CONTACT_US_URL = ROOT + CONTACT_US;
 
 	@RequestMapping(value = { ROOT, HOME_URL, INDEX_URL }, method = RequestMethod.GET)
 	public String indexPage() {
+		logger.info("get " + INDEX + " page");
 		return INDEX;
 	}
 
 	@RequestMapping(value = { CATALOG_URL }, method = RequestMethod.GET)
 	public String catalogPage() {
+		logger.info("get " + CATALOG + " page");
 		return CATALOG;
 	}
 
 	@RequestMapping(value = { ADVANCED_SEARCH_URL }, method = RequestMethod.GET)
 	public String advancedSearchPage() {
+		logger.info("get " + ADVANCED_SEARCH + " page");
 		return ADVANCED_SEARCH;
 	}
 
 	@RequestMapping(value = { ABOUT_US_URL }, method = RequestMethod.GET)
 	public String aboutUsPage() {
+		logger.info("get " + ABOUT_US + " page");
 		return ABOUT_US;
 	}
-	
+
 	@RequestMapping(value = { CONTACT_US_URL }, method = RequestMethod.GET)
 	public String contactUsPage() {
+		logger.info("get " + CONTACT_US + " page");
 		return CONTACT_US;
 	}
-
 
 }
